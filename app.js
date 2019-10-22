4a8f87afbeaf56a1b594e1d3ef396eb35ede6f3a
Hidenori Miyoshi <miyohide@gmail.com>
2019-10-22 10:36:47 +0900

    P46-47. フィルタ処理例を追加

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ var vm = new Vue({
     el: '#app',
     data: {
         items: items
+    },
+    filters: {
+        numberWithDelimiter: function (value) {
+            if (!value) {
+                return '0'
+            }
+            return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1, ')
+        }
     }
 })
 // 書籍では書かれていたがブラウザで試す上では不要
