a0767445cdf7a9282068120aa6a8e74fa165180f
Hidenori Miyoshi <miyohide@gmail.com>
2019-11-23 16:48:53 +0900

    P254. create main

---
+++ ch07/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+
+new Vue({
+    el: '#app',
+    // コンポーネントからストアを利用できるようにする
+    store,
+
+    render: h => h(App)
+})
