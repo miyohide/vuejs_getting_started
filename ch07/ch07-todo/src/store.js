458479e4b77fdd20ecfbc76d26f8ba26ee9c5223
Hidenori Miyoshi <miyohide@gmail.com>
2019-11-24 16:14:18 +0900

    7.5.4 ラベルの追加

--- ch07/ch07-todo/src/store.js
+++ ch07/ch07-todo/src/store.js
@@ -5,27 +5,50 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // タスクの初期ステート
     tasks: [
       {
         id: 1,
         name: '牛乳を買う',
+        labelIds: [1, 2],
         done: false
       },
       {
         id: 2,
         name: 'Vue.jsの本を買う',
+        labelIds: [1, 3],
         done: true
       }
     ],
+
+    // ラベルの初期ステート
+    labels: [
+      {
+        id: 1,
+        text: '買い物'
+      },
+      {
+        id: 2,
+        text: '食料'
+      },
+      {
+        id: 3,
+        text: '本'
+      }
+    ],
+
     // 次に追加するタスクのID。実際にアプリではサーバーなどで生成する必要あり。
     nextTaskId: 3,
+    // 次に追加するラベルのID。実際のアプリではサーバーなどで生成する必要あり。
+    nextLabelId: 4,
   },
   mutations: {
     // タスクを追加する
-    addTask (state, { name }) {
+    addTask (state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
         name,
+        labelIds,
         done: false
       })
       // 次に追加するタスクのIDを更新する
@@ -41,6 +64,15 @@ export default new Vuex.Store({
         task.done = !task.done
       })
     },
+    // ラベルを追加する
+    addLabel (state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+      // 次に追加するラベルに付与するIDをを更新する
+      state.nextLabelId++
+    }
 
   },
   actions: {
