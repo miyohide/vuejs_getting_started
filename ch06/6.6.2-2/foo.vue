7d5d209ed96ff2cfbb4e85ba32130fa0210fbc94
Hidenori Miyoshi <miyohide@gmail.com>
2019-12-02 21:41:40 +0900

    fix typo.

--- ch06/6.6.2-2/foo.vue
+++ ch06/6.6.2-2/foo.vue
@@ -5,6 +5,11 @@
     </div>
 </template>
 
+<!-- scopedがついていないので、以下のtext-decorationは全体に適用される -->
+<style>
+p { text-decoration: underline; }
+</style>
+
 <style scoped>
 .foo { border: solid 1px green; margin: 4px; padding: 4px; }
 .header { font-size: 150%; }
