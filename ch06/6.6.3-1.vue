4ac7c1a6b4f0abb3b3833e8a3952a9314ac0aaf2
Hidenori Miyoshi <miyohide@gmail.com>
2019-12-04 18:42:31 +0900

    CSSモジュールのサンプルを追加

---
+++ ch06/6.6.3-1.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- スコープ付きCSSとはclassの指定の仕方が異なることに注意 -->
+    <p :class="$style.message">こんにちは</p>
+</template>
+
+<script>
+export default {
+    created () {
+        // ESLintのno-consoleルールを無視するために以下のコメントをつけている
+        // eslint-disable-next-line no-console
+        console.log('css modules: $style', this.$style) // CSSモジュールでは$styleでJavaScriptからも参照できる
+    }
+}
+</script>
+
+<style module>
+.message { color: #42b983; }
+</style>
