6a789f8d367265d21269e7f58d3e2a599c496322
Hidenori Miyoshi <miyohide@gmail.com>
2019-12-30 09:34:28 +0900

    fix quotes

--- ch08-10/kanban-app/test/unit/specs/components/molecules/KbnLoginForm.spec.js
+++ ch08-10/kanban-app/test/unit/specs/components/molecules/KbnLoginForm.spec.js
@@ -1,4 +1,4 @@
-import { mount } from "@vue/test-utils"
+import { mount } from '@vue/test-utils'
 import KbnLoginForm from '@/components/molecules/KbnLoginForm.vue'
 
 describe('KbnLoginForm', () => {
