66d4dc53e09032086fd16b540614916edd4115d9
Hidenori Miyoshi <miyohide@gmail.com>
2020-01-05 21:30:25 +0900

    define mutation type

---
+++ kanban-app/src/store/mutation-types.js
@@ -0,0 +1,12 @@
+// ユーザーがログインする
+export const AUTH_LOGIN = 'AUTH_LOGIN'
+// 全タスクリストを取得する
+export const FETCH_ALL_TASKLIST = 'FETCH_ALL_TASKLIST'
+// タスクを追加する
+export const ADD_TASK = 'ADD_TASK'
+// タスクを更新する
+export const UPDATE_TASK = 'UPDATE_TASK'
+// タスクを削除する
+export const REMOVE_TASK = 'REMOVE_TASK'
+// ユーザーがログアウトする
+export const AUTH_LOGOUT = 'AUTH_LOGOUT'
