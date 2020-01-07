107ff3aa6d7a108601b9da26625a11e372291433
Hidenori Miyoshi <miyohide@gmail.com>
2020-01-07 22:32:46 +0900

    create first KbnButton.vue

---
+++ kanban-app/src/components/atoms/KbnButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    :class="classes"
+    type="button"
+  >
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'KbnButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    }
+  },
+
+  computed: {
+    // 参考 https://jp.vuejs.org/v2/guide/class-and-style.html#%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E6%A7%8B%E6%96%87
+    classes () {
+      const cls = this.type === 'text' ? ('-' + this.type) : ''
+      return [`kbn-button${cls}`]
+    }
+  }
+}
+</script>
