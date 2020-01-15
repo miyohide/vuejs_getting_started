0f1deec48594668aa714a1289b58d3819bc14d5b
Hidenori Miyoshi <miyohide@gmail.com>
2020-01-15 22:01:08 +0900

    implement format validation.

--- kanban-app/src/components/molecules/KbnLoginForm.vue
+++ kanban-app/src/components/molecules/KbnLoginForm.vue
@@ -23,6 +23,7 @@
 
 <script>
 import KbnButton from '@/components/atoms/KbnButton.vue'
+const REGEX_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const required = val => !!val.trim()
 export default {
   name: 'KbnLoginForm',
@@ -49,6 +50,7 @@ export default {
       return {
         email: {
           required: required(this.email),
+          format: REGEX_EMAIL.test(this.email)
         },
       }
     },
