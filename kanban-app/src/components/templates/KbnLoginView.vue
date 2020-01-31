a0bd5b643bd777bc622432d6db29bda36a0ba230
Hidenori Miyoshi <miyohide@gmail.com>
2020-01-31 23:40:20 +0900

    テストの書き方がよくわからないので、とりあえず書籍通りに実装

---
+++ kanban-app/src/components/templates/KbnLoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-view">
+    <h1>Kanban App</h1>
+    <KbnLoginForm :onlogin="handleLogin" />
+  </div>
+</template>
+
+<script>
+import KbnLoginForm from '@/components/molecules/KbnLoginForm.vue'
+
+export default {
+  name: 'KbnLoginView',
+
+  components: {
+    KbnLoginForm
+  },
+
+  methods: {
+    handleLogin (authInfo) {
+    }
+  }
+}
+</script>
